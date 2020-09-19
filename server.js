54882a13143e699044c711978a5f852e2aa0b1db
unknown <diego.resendez@yahoo.com>
2020-09-19 12:50:30 -0700

    PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 1000;
+const PORT =process.env.PORT || 1000;
 
 const app = express();
 
